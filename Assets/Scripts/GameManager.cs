5de5b730228489e339f4e5427ae0ab98fcc51e73
Alstn1012 <kimminsu101220062@gmail.com>
2023-09-01 19:23:39 +0900

    aa
    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,7 +10,34 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public PoolManager poolManager;
 
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
     public int score = 0;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 5);
+        Instantiate(enemyObjs[ranEnemy],
+                    spawnPoints[ranPoint].position,
+                    spawnPoints[ranPoint].rotation);
+    }
 
     private void Awake()
     {
