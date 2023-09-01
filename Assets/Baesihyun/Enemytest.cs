debee8737d6fe24a6640f401156d873c75ce6f48
BshAIdev <96278204+BshAIdev@users.noreply.github.com>
2023-09-01 19:33:35 +0900

    a
    a

--- Assets/Baesihyun/Enemytest.cs
+++ Assets/Baesihyun/Enemytest.cs
@@ -1,7 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemytest : MonoBehaviour
 {
+    public float speed;
+    
+    public int hp;
+
+    Rigidbody2D rigid;
+    GameObject pl;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        pl = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 dir = pl.transform.position - transform.position;
+        rigid.velocity = dir * speed;
+    }
+
+
+
 }
