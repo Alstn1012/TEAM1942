b810f705d767a488b5424e789800c70ecea470e6
Alstn1012 <kimminsu101220062@gmail.com>
2023-09-10 03:20:37 +0900

    Update GameOverPanel.cs

--- Assets/Scripts/GameOverPanel.cs
+++ Assets/Scripts/GameOverPanel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class GameOverPanel : MonoBehaviour
 {
@@ -11,12 +10,11 @@ public class GameOverPanel : MonoBehaviour
     public Text Text_GameMaxResult;
 
     private string KeyName = "bestScore";
-    public int highScore;
+    public int highScore = 0;
 
     void Awake()
     {
-        highScore = PlayerPrefs.GetInt(KeyName, GameManager.instance.score);
-        Text_GameMaxResult.text = $"HIGH SCORE : {highScore.ToString()}";
+
     }
 
     public void Show()
@@ -24,10 +22,14 @@ public class GameOverPanel : MonoBehaviour
         GameManager.isGameEnd = true;
         int score = GameManager.instance.score;
         transform.gameObject.SetActive(true);
-        
+
+        // 최고 점수 불러오기
+        highScore = PlayerPrefs.GetInt(KeyName, 0);
+
         if (score > highScore)
         {
             PlayerPrefs.SetInt(KeyName, score);
+            highScore = score; // 최고 점수 갱신
         }
 
         Text_GameResult.text = "SCORE : " + score.ToString();
