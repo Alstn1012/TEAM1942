a1cd64076725a9b0d411ddf366c292d38743adb9
Alstn1012 <kimminsu101220062@gmail.com>
2023-09-10 14:38:20 +0900

    a
    a

---
+++ Assets/Scripts/TextBlink.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SpaceKey_Start : MonoBehaviour
+{
+
+    Text flashingText;
+
+    void Start()
+    {
+        flashingText = GetComponent<Text>();
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            flashingText.text = "";
+            yield return new WaitForSeconds(.8f);
+            flashingText.text = "Press to Start";
+            yield return new WaitForSeconds(.8f);
+        }
+    }
+}
